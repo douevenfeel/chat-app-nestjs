72750a4163c7512c4af5e8756321681cebf8def0
Dmitriy Lobanov <citrusbound@mail.ru>
2023-04-07 00:10:33 +0300

    add validation for firstname and lastname

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+import {
+    IsEmail,
+    IsString,
+    Matches,
+    MaxLength,
+    MinLength,
+} from 'class-validator';
 
 export class CreateUserDto {
     @ApiProperty({ example: 'user@mail.ru', description: 'Почта' })
@@ -17,10 +23,18 @@ export class CreateUserDto {
     })
     readonly password: string;
 
+    @Matches(/^[a-zA-Zа-яА-Я]+$/, {
+        message:
+            'Имя может состоять только из букв русского иили латинского алфавита',
+    })
     @ApiProperty({ example: '12345', description: 'Имя' })
     @IsString({ message: 'Имя должно быть строкой' })
     readonly firstName: string;
 
+    @Matches(/^[a-zA-Zа-яА-Я]+$/, {
+        message:
+            'Имя может состоять только из букв русского иили латинского алфавита',
+    })
     @ApiProperty({ example: '12345', description: 'Фамилия' })
     @IsString({ message: 'Фамилия должна быть строкой' })
     readonly lastName: string;
